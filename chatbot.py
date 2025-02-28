96b0316331a3f1180c765f81b8adba6fb2527789
Devi Saranya <149192257+SaranyaYanamadala@users.noreply.github.com>
2025-02-28 13:52:14 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,30 @@
+import streamlit as st
+import google.generativeai as genai
 
+API_key = "AIzaSyA1G168bJfFFdzBKv8Z4vmyWLP9mAtOZIg"
+genail.configure(api_key = API_key)
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+if "chat" not in st.session_state:
+    st.session_state.chat = model.start_chat(history=[])
+
+st.title("🤖 Chatbot - Your AI Assistant")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if promt := st.chat_input("Say something....") :
+  ## Add user message to the chat history
+  st.session_state.mesages.append({"role" : "user", "content" : prompt})
+  with st.chat_message("user") :
+    st.markdown(prompt)
+
+  response = st.session_state.chat.send_message(prompt)
+
+  st.session_state.messages.append({"role" : "assistant", "content" : response.text})
+  with st.chat_message("assistant") :
+    st.markdown(response.text)
